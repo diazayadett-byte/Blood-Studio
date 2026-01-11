c28499362058c32994db422d1de36c45b1f27c53
Ayadett Diaz <diazayadett@gmail.com>
2026-01-11 11:48:39 -0600

    fix: correccion de import del header

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,11 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  // Asegúrate de que el return envuelva todo tu HTML/JSX
   return (
     <>
       <div>
         <Header />
-
-        
       </div>
     </>
   )
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import logoBloom from '../assets/logo.png';
+
 import "./Header.css";
 
 
